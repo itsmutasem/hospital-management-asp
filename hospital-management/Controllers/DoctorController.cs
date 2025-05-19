2fe533e7f025804ae6278e5c51bb6ed82aa87f98
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-05-19 14:42:24 +0300

    Add Static Data in the Doctor Controller

--- hospital-management/Controllers/DoctorController.cs
+++ hospital-management/Controllers/DoctorController.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using hospital_management.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace hospital_management.Controllers;
 
 public class DoctorController : Controller
 {
-    
+    private static List<Doctor> doctors = new List<Doctor>
+    {
+        new Doctor { DoctorId = 1, FullName = "Dr. Sarah Lee", Specialty = "Cardiology" },
+        new Doctor { DoctorId = 2, FullName = "Dr. Ahmed Khan", Specialty = "Orthopedics" },
+        new Doctor { DoctorId = 3, FullName = "Dr. Maria Ruiz", Specialty = "Dermatology" },
+    };
     public IActionResult Index()
     {
         return View();
--- hospital-management/Models/Doctor.cs
+++ hospital-management/Models/Doctor.cs
@@ -3,6 +3,6 @@
 public class Doctor
 {
     public int DoctorId { get; set; }
-    public string DoctorName { get; set; }
+    public string FullName { get; set; }
     public string Specialty { get; set; }
 }
