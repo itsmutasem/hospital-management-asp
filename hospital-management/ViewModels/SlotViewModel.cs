b26b4424761ee3370472e7bfa98567b6763af261
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-05-19 13:40:09 +0300

    Create SlotViewModel and Define Data

---
+++ hospital-management/ViewModels/SlotViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace hospital_management.ViewModels;
+
+public class SlotViewModel
+{
+    public DateTime Day { get; set; }
+    public string Time { get; set; }
+    public int DoctorId { get; set; }
+    public string DoctorName { get; set; }
+    public string Major { get; set; }
+}
